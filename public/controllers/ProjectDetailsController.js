d21079ff6f472b6eb02f3a2163af240475ee9f99
a36857 <36857@alunos.isel.ipl.pt>
2016-09-12 12:22:02 +0100

    nfoxapp data

--- public/controllers/ProjectDetailsController.js
+++ public/controllers/ProjectDetailsController.js
@@ -2,20 +2,16 @@ angular.module("mainApp")
     .controller('ProjectDetailsController',function($scope,$routeParams,$http,$sce){
         var url = "/projects/" + $routeParams.id;
 
-        $('#spinner').show();
         $http.get(url)
              .success( function(rsp) {
-                  $scope.imageFirst = rsp.images.shift();;
+                  $scope.imageFirst = rsp.images.shift();
                   $scope.images = rsp.images;
 
                   $scope.project = rsp;
 
                   $scope.video = $sce.trustAsResourceUrl(rsp.video);
-
-                  $('#spinner').hide();
              })
              .error(function() {
                   BootstrapDialog.alert('Some error occurred. Try again later');
-                  $('#spinner').hide();
              });
     });
